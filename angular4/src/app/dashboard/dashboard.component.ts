efa4e38a74a5ea42ade4e9bae5ac9cff158fa7f4
Sergio Peluzzi <sergiopeluzzi@gmail.com>
2017-08-31 09:55:50 -0400

    testando ;D

--- angular4/src/app/dashboard/dashboard.component.ts
+++ angular4/src/app/dashboard/dashboard.component.ts
@@ -15,12 +15,11 @@ export class DashboardComponent {
   constructor(http: Http) { //Injetando a dependência do Http no constructor. Sempre que o component for carregado o Http será instanciado
     const url = 'http://localhost:3003/api/billingSummary'
     http.get(url) //Envia um GET para a url passada
-      .map(res => res.json()) //recebe a resposta em uma callback e mapeia pra json()
-      .subscribe(({credit = 0, debt = 0}) => { //Operador destructoring | Pega o objeto da resposta e desestrutura nas variáveis passadas
+      .map(res => res.json()) //recebe a resposta do get e passa em uma callback q mapeia pra json()
+      .subscribe(({credit = 0, debt = 0}) => { //Operador destructoring | Pega o objeto da resposta, agora em JSON e desestrutura nas variáveis passadas
         this.credit = credit
         this.debt = debt
         this.total = credit - debt
       })
   }
-
 }
